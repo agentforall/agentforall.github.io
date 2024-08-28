03628674c9cf9b96fbc72bf0f4181885fd328a38
agentforall <140298961+agentforall@users.noreply.github.com>
2024-08-29 00:10:25 +0530

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -11,42 +11,50 @@ export const data: {
     {
         name: "Upendra Khanna",
         type: "Property Broker",
-        rating: 4.32
+        rating: 4.32,
+        index: 0
     },
     {
         name: "Indira Shukla",
         type: "Health Agent",
-        rating: 3.78
+        rating: 3.78,
+        index: 1
     },
     {
         name: "Jaya Banerjee",
         type: ["Travel Agent", "Property Broker"],
-        rating: 4.96
+        rating: 4.96,
+        index: 2,
     },
     {
         name: "Bhagwati Dutta",
         type: "Stock Broker",
-        rating: 2.64
+        rating: 2.64,
+        index: 3
     },
     {
         name: "Rahul Sharma",
         type: ["Legal Agent", "Advertising Agent"],
-        rating: 3.89
+        rating: 3.89,
+        index: 4
     },
     {
         name: "Aaditya Joshi",
         type: "Insurance Agent",
-        rating: 4.15
+        rating: 4.15,
+        index: 5
     },
     {
         name: "Ashlesh Talwar",
         type: "Travel Agent",
-        rating: 5.00
+        rating: 5.00, 
+        index: 6
     },
     { 
         name: "Rajinder Trivedi",
         type: "Insurance Agent",
-        rating: 4.27
+        rating: 4.27,
+        index: 7
     }
 ];
 
